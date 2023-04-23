60ed825ae865293d754ca9e6823ea568d3858dd8
Nico Martinucci <domenico.martinucci@gmail.com>
2023-04-23 09:48:44 -0700

    add placeholder homepage

---
+++ src/HomePage.tsx
@@ -0,0 +1,9 @@
+import { Container, Typography } from "@mui/material";
+
+export default function HomePage() {
+    return (
+        <Container>
+            <Typography variant="h1">Home!</Typography>
+        </Container>
+    )
+}
--- src/RoutesList.tsx
+++ src/RoutesList.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom"
 import RecipesHome from "./RecipesHome"
 import Recipe from "./Recipe"
+import HomePage from "./HomePage"
 
 export default function RoutesList() {
     return (
         <Routes>
+            <Route path="/" element={<HomePage />} />
             <Route path="/recipes" element={<RecipesHome />} />
             <Route path="/recipes/:recipe_id" element={<Recipe />} />
         </Routes>
