fd1ddc296c79953571cbc70b8e02e7d3e4d32fac
Nico Martinucci <domenico.martinucci@gmail.com>
2023-04-26 10:36:22 -0700

    adjust formatting to accomodate no amounts

--- src/RecipeItem.tsx
+++ src/RecipeItem.tsx
@@ -6,7 +6,7 @@ interface Props {
 }
 
 export default function RecipeItem({ item }: Props) {
-    let itemText = item.amount
+    let itemText = item.amount ? item.amount : ""
     itemText = item.unit ? (itemText + " " + item.unit) : itemText
     itemText = item.ingredient ? (itemText + " " + item.ingredient) : itemText
     itemText = item.description ? (itemText + ", " + item.description) : itemText
