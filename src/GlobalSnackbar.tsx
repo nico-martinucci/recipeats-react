34a487863589ad00b2195a20e6c67a535d0cee92
Nico Martinucci <domenico.martinucci@gmail.com>
2023-05-11 11:43:12 -0800

    [wip] add snackbar and context

---
+++ src/GlobalSnackbar.tsx
@@ -0,0 +1,27 @@
+import { forwardRef } from 'react';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert, { AlertProps } from '@mui/material/Alert';
+
+interface Props {
+    open: boolean;
+    handleClose: () => void;
+}
+
+const Alert = forwardRef<HTMLDivElement, AlertProps>(function Alert(
+    props,
+    ref,
+) {
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
+export default function GlobalSnackbar({ open, handleClose }: Props) {
+    return (
+        <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+            <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
+                This is a success message!
+            </Alert>
+        </Snackbar>
+    )
+}
